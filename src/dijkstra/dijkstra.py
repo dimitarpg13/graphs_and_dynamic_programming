3ec9064d5b4bce1267f33c3167c7c1d29b2078ab
Dimitar Gueorguiev <dimitar_pg13@hotmail.com>
2024-09-07 22:14:16 -0400

    update docs

--- src/dijkstra/dijkstra.py
+++ src/dijkstra/dijkstra.py
@@ -1,14 +1,13 @@
 import sys
 
 
-class DijkstraIEDF:
+class DijkstraIEFD:
     """
-    Iteratively Expanding Distance Front Algorithm (IEDF)
+    Iteratively Expanding Front of Distances Algorithm (IEFD)
     """
-    def __init__(self, vertex_count):
+    def __init__(self, vertex_count, adjacency_matrix):
         self.vertex_count = vertex_count
-        self.graph = [[0 for _ in range(vertex_count)]
-                      for _ in range(vertex_count)]
+        self.adjacency_matrix = adjacency_matrix
 
     # A utility function to find the vertex with
     # minimum distance value, from the set of vertices
@@ -20,7 +19,67 @@ class DijkstraIEDF:
         min_index = -1
         # Search nearest vertex not in the shortest path tree
         for k in range(self.vertex_count):
-            if dist[k] < min_dist and spt_set[k] == False:
+            if dist[k] < min_dist and spt_set[k] is False:
+                min_dist = dist[k]
+                min_index = k
+
+        return min_index
+
+    # Function that implements Dijkstra's single source
+    # shortest path algorithm for a graph represented
+    # using adjacency matrix representation
+    def run(self, s):
+        """
+        Run Dijkstra's algorithm obtaining shortest paths from a given starting element with index s
+        :param s: start index
+        :return: list of shortest path lengths
+        """
+        dist = [sys.maxsize] * self.vertex_count
+        dist[s] = 0
+        spt_set = [False] * self.vertex_count
+
+        for _ in range(self.vertex_count):
+
+            # Pick the minimum distance vertex from
+            # the set of vertices not yet processed.
+            # x is always equal to t in first iteration
+            x = self.min_distance(dist, spt_set)
+
+            # Put the minimum distance vertex in the
+            # shortest path tree
+            spt_set[x] = True
+
+            # Update dist value of the adjacent vertices
+            # of the picked vertex only if the current
+            # distance is greater than new distance and
+            # the vertex in not in the shortest path tree
+            for y in range(self.vertex_count):
+                if self.adjacency_matrix[x][y] > 0 and spt_set[y] is False and \
+                        dist[y] > dist[x] + self.adjacency_matrix[x][y]:
+                    dist[y] = dist[x] + self.adjacency_matrix[x][y]
+
+        return dist
+
+
+class DijkstraIEBD:
+    """
+    Iteratively Expanding Backwards Distance Algorithm (IEBD)
+    """
+    def __init__(self, vertex_count, adjacency_matrix):
+        self.vertex_count = vertex_count
+        self.adjacency_matrix = adjacency_matrix
+
+    # A utility function to find the vertex with
+    # minimum distance value, from the set of vertices
+    # not yet included in shortest path tree
+    def min_distance(self, dist, spt_set):
+
+        # Initialize minimum distance for next node
+        min_dist = sys.maxsize
+        min_index = -1
+        # Search nearest vertex not in the shortest path tree
+        for k in range(self.vertex_count):
+            if dist[k] < min_dist and spt_set[k] is False:
                 min_dist = dist[k]
                 min_index = k
 
@@ -30,7 +89,11 @@ class DijkstraIEDF:
     # shortest path algorithm for a graph represented
     # using adjacency matrix representation
     def run(self, t):
-
+        """
+        Run Dijkstra's algorithm for obtaining shortest paths to a given target element with index t
+        :param t: target index
+        :return: list of shortest path lengths
+        """
         dist = [sys.maxsize] * self.vertex_count
         dist[t] = 0
         spt_set = [False] * self.vertex_count
@@ -51,8 +114,52 @@ class DijkstraIEDF:
             # distance is greater than new distance and
             # the vertex in not in the shortest path tree
             for y in range(self.vertex_count):
-                if self.graph[x][y] > 0 and spt_set[y] == False and \
-                        dist[y] > dist[x] + self.graph[x][y]:
-                    dist[y] = dist[x] + self.graph[x][y]
+                if self.adjacency_matrix[x][y] > 0 and spt_set[y] is False and \
+                        dist[y] > dist[x] + self.adjacency_matrix[x][y]:
+                    dist[y] = dist[x] + self.adjacency_matrix[x][y]
 
         return dist
+
+
+
+import heapq
+
+# iPair ==> Integer Pair
+iPair = tuple
+
+
+class DijkstraIEDFOpt:
+    """
+    Iteratively Expanding Distance Front Optimized Algorithm (IEDFOpt)
+    """
+    def __init__(self, vertex_count):
+        self.vertex_count = vertex_count
+        self.adjacency_matrix = [[] for _ in range(vertex_count)]
+
+    def addEdge(self, u: int, v: int, w: int):
+        self.adjacency_matrix[u].append((v, w))
+        self.adjacency_matrix[v].append((u, w))
+
+    def run(self, t):
+        """
+        Computes the shortest paths from t to all other nodes
+        :param t: initial node
+        :return: list of distances from t to all other nodes
+        """
+        # vector for the distances and initialize the distance of t
+        dist = [sys.maxsize] * self.vertex_count
+        dist[t] = 0
+
+        # priority queue storing vertices that are being preprocessed
+        pq = []
+        heapq.heappush(pq, (0, t))
+
+class DijkstraDP:
+    """
+    Dijsktra's algorithm implemented via Dynamic Programming
+    """
+    def __init__(self, vertex_count, adjacency_matrix):
+        self.vertex_count = vertex_count
+        self.adjacency_matrix = adjacency_matrix
+        self.dist = [sys.maxsize] * self.vertex_count
+        self.dist[vertex_count] = 0
--- src/dijkstra/tests/test_dijkstra.py
+++ src/dijkstra/tests/test_dijkstra.py
@@ -5,8 +5,8 @@ from dijkstra.dijkstra import DijkstraIEDF
 class DijkstraTestCase(unittest.TestCase):
 
     def test_dijkstra_naive_impl(self):
-        g = DijkstraIEDF(9)
-        g.graph = [[0, 4, 0, 0, 0, 0, 0, 8, 0],
+
+        adj_matr = [[0, 4, 0, 0, 0, 0, 0, 8, 0],
                    [4, 0, 8, 0, 0, 0, 0, 11, 0],
                    [0, 8, 0, 7, 0, 4, 0, 0, 2],
                    [0, 0, 7, 0, 9, 14, 0, 0, 0],
@@ -14,8 +14,10 @@ class DijkstraTestCase(unittest.TestCase):
                    [0, 0, 4, 14, 10, 0, 2, 0, 0],
                    [0, 0, 0, 0, 0, 2, 0, 1, 6],
                    [8, 11, 0, 0, 0, 0, 1, 0, 7],
-                   [0, 0, 2, 0, 0, 0, 6, 7, 0]
-                   ]
+                   [0, 0, 2, 0, 0, 0, 6, 7, 0]]
+
+        g = DijkstraIEDF(9,adj_matr)
+
         dist = g.run(0)
 
         self.assertEqual(dist[0], 0)  # add assertion here
