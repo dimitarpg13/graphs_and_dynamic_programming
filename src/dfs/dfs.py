1186be663af1b2aef3b30b68b6a359d0b00d3a79
Dimitar Gueorguiev <dimitar_pg13@hotmail.com>
2024-09-08 23:21:22 -0400

    update code

--- src/dfs/dfs.py
+++ src/dfs/dfs.py
@@ -1,6 +1,23 @@
 import sys
 
 
-class DepthFirstSearch:
-    def __init__(self):
-        pass
+class DepthFirstSearchUndirectedGraph:
+    def __init__(self, adj):
+        """
+        The undirected graph is given as an adjacency list
+        :param adj: adjacency list
+        """
+        self.adj = adj
+
+    def visit_recursive(self, start, visited, visitations):
+        visited[start] = True
+        visitations.append(start)
+        for i in self.adj[start]:
+            if not visited[i]:
+                self.visit_recursive(i, visited, visitations)
+
+    def run(self):
+        visitations = []
+        visited = [False] * len(self.adj)
+        self.visit_recursive(0, visited, visitations)
+        return visitations
