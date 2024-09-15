5a62674ca666ec125ac996fa658906316606aca8
Dimitar Gueorguiev <dimitar_pg13@hotmail.com>
2024-09-15 14:01:48 -0400

    update code

---
+++ src/bfs/bfs.py
@@ -0,0 +1,42 @@
+from collections import deque
+
+
+class BreadthFirstSearch:
+    """
+    Breadth First Search using deque
+    """
+    def __init__(self, adj_list, num_nodes):
+        """
+        :param adj_list: adjacency list
+        :param num_nodes: number of nodes
+        """
+        self.adj_list = adj_list
+        self.num_nodes = num_nodes
+        self.visited = [False] * self.num_nodes
+
+    def run(self, start):
+        q = deque()
+
+        self.visited[start] = True
+        q.append(start)
+        stack = []
+        while q:
+            current = q.popleft()
+            stack.append(current)
+            print(current)
+            for v in self.adj_list[current]:
+                if v and not self.visited[v]:
+                    self.visited[v] = True
+                    q.append(v)
+
+        return stack
+
+
+if __name__ == '__main__':
+    adj_list = [[0, 1], [1, 2], [2, 3], [3, 4], [4, 5], [5, None]]
+    num_nodes = 6
+    bfs = BreadthFirstSearch(adj_list, num_nodes)
+    res = bfs.run(0)
+    print(res)
+
+
--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,6 +1,35 @@
 
 
-def adj_matrix_to_adj_list(adj_matrix):
+def arc_list_to_adj_list(arc_list, num_nodes):
+    """
+    converts arc list to adjacency list
+    :param arc_list: list of arcs
+      Example: [[0, 1], [1, 2], [3, 1], [3, 2]]
+    :param num_nodes: number of nodes
+    :return:adj_list: adjacency list
+    """
+    adj_list = [[] for _ in range(num_nodes)]
+    for arc in arc_list:
+        adj_list[arc[0]].append(arc[1])
+    return adj_list
+
+
+def edge_list_to_adj_list(edge_list, num_nodes):
+    """
+    converts edge list to adjacency list
+    :param edge_list: list of arcs
+      Example: [[0, 1], [1, 2], [3, 1], [3, 2]]
+    :param num_nodes: number of nodes
+    :return:adj_list: adjacency list
+    """
+    adj_list = [[] for _ in range(num_nodes)]
+    for edge in edge_list:
+        adj_list[edge[0]].append(edge[1])
+        adj_list[edge[1]].append(edge[0])
+    return adj_list
+
+
+def adj_matrix_to_adj_list(adj_matrix, num_nodes):
     pass
 
 
